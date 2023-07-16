69e62cabdb8a042a0f9ff652b567b0a15296a8db
Esteban Salas <estebansalas2502@gmail.com>
2023-07-16 10:29:01 -0600

    Proyecto base

---
+++ ProyectoProgramacionV/Controllers/Gestion_deportivaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgramacionV.Controllers
+{
+    public class Gestion_deportivaController : Controller
+    {
+        // GET: Gestion_deportiva
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
